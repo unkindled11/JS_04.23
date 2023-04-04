e378b27f01fd3db0fa15b6f7bb7cd5af6edec60e
Dmytro Kulakov <gambler11kulakov@gmail.com>
2023-04-04 15:21:22 +0300

    [1.1:04.04] finished first task

---
+++ index.js
@@ -0,0 +1,67 @@
+// === Task#1 ===
+
+function isTrue() {
+  let sign = prompt("Please enter value");
+  if (Math.sign(sign) == -1) {
+    console.log("Incorrect input");
+    return isTrue();
+  } else if (sign % 1 === 0) {
+    console.log(`Number: ${toNumber(sign)}`);
+    console.log(`Factorial: ${factorial(sign)}`);
+    console.log(`Square: ${sign * sign}`);
+    console.log(`isPrime: ${isPrime(sign)}`);
+    console.log(`isEven: ${isEven(sign)}`);
+    console.log(`Delimiters: ${delimiters(sign)}`);
+  } else {
+    console.log("Incorrect input");
+    return isTrue();
+  }
+}
+
+function toNumber(n) {
+  return Number(n);
+}
+
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  }
+  return n != 1 ? n * factorial(n - 1) : 1;
+}
+
+function isPrime(n) {
+  if (n < 2) {
+    return "Число должно быть больше 1";
+  } else if (n === 2) {
+    return "True";
+  }
+
+  let i = 2;
+  const limit = Math.sqrt(n);
+  while (i <= limit) {
+    if (n % i === 0) {
+      return "False";
+    }
+    i += 1;
+  }
+
+  return "True";
+}
+
+function isEven(n) {
+  return n % 2 === 0 ? "True" : "False";
+}
+
+function delimiters(n) {
+  let dlmtrs = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      dlmtrs.push(i);
+    }
+  }
+  return dlmtrs.join(" ");
+}
+
+isTrue();
+
+// === Task#2 ===
