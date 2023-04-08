6024b5462a015be292731dd283bb07397e4f8a82
Dmytro Kulakov <gambler11kulakov@gmail.com>
2023-04-08 14:15:00 +0300

    [02.02:08.04]: finished second task

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 // ===Task#1===
-function makeDeepClone(obj) {
-  if (arguments.length === 0 || obj === null) {
-    throw new Error("It's empty");
-  } else if (obj instanceof Array) {
-    throw new Error("It's an Array");
-  } else if (obj == null || typeof obj !== "object") {
-    return obj;
-  }
-  if (obj instanceof Date) {
-    return new Date(obj);
-  } else if (obj instanceof String) {
-    return new String(obj);
-  } else if (obj instanceof Number) {
-    return new Number(obj);
-  } else if (obj instanceof Boolean) {
-    return new Boolean(obj);
-  } else if (obj instanceof RegExp) {
-    return new RegExp(obj);
-  }
+// function makeDeepClone(obj) {
+//   if (arguments.length === 0 || obj === null) {
+//     throw new Error("It's empty");
+//   } else if (obj instanceof Array) {
+//     throw new Error("It's an Array");
+//   } else if (obj == null || typeof obj !== "object") {
+//     return obj;
+//   }
+//   if (obj instanceof Date) {
+//     return new Date(obj);
+//   } else if (obj instanceof String) {
+//     return new String(obj);
+//   } else if (obj instanceof Number) {
+//     return new Number(obj);
+//   } else if (obj instanceof Boolean) {
+//     return new Boolean(obj);
+//   } else if (obj instanceof RegExp) {
+//     return new RegExp(obj);
+//   }
 
-  let clone = {};
+//   let clone = {};
 
-  for (let key in obj) {
-    clone[key] = makeDeepClone(obj[key]);
+//   for (let key in obj) {
+//     clone[key] = makeDeepClone(obj[key]);
+//   }
+//   return clone;
+// }
+
+// makeDeepClone(obj);
+
+// ===Task#2===
+function selectFromInterval(arr, x, y) {
+  if (
+    arr.length === 0 ||
+    arr.some((i) => typeof i !== "number") ||
+    typeof x != "number" ||
+    typeof y != "number"
+  ) {
+    throw new Error("xx");
+  } else if (x < y) {
+    return arr.filter((i) => i >= x && i <= y);
+  } else if (y < x) {
+    return arr.filter((i) => i >= y && i <= x);
   }
-  return clone;
 }
 
-makeDeepClone(obj);
+// ===Task#3===
