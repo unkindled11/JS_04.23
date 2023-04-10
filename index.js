b4770a7c37ec9b148dfcb9d934d754cde660b9a6
Dmytro Kulakov <gambler11kulakov@gmail.com>
2023-04-10 10:22:06 +0300

    [2.03:10.04]: finished third task

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 // ===Task#1===
-// function makeDeepClone(obj) {
-//   if (arguments.length === 0 || obj === null) {
-//     throw new Error("It's empty");
-//   } else if (obj instanceof Array) {
-//     throw new Error("It's an Array");
-//   } else if (obj == null || typeof obj !== "object") {
-//     return obj;
-//   }
-//   if (obj instanceof Date) {
-//     return new Date(obj);
-//   } else if (obj instanceof String) {
-//     return new String(obj);
-//   } else if (obj instanceof Number) {
-//     return new Number(obj);
-//   } else if (obj instanceof Boolean) {
-//     return new Boolean(obj);
-//   } else if (obj instanceof RegExp) {
-//     return new RegExp(obj);
-//   }
+function makeDeepClone(obj) {
+  if (arguments.length === 0 || obj === null) {
+    throw new Error("It's empty");
+  } else if (obj instanceof Array) {
+    throw new Error("It's an Array");
+  } else if (obj == null || typeof obj !== "object") {
+    return obj;
+  }
+  if (obj instanceof Date) {
+    return new Date(obj);
+  } else if (obj instanceof String) {
+    return new String(obj);
+  } else if (obj instanceof Number) {
+    return new Number(obj);
+  } else if (obj instanceof Boolean) {
+    return new Boolean(obj);
+  } else if (obj instanceof RegExp) {
+    return new RegExp(obj);
+  }
 
-//   let clone = {};
+  let clone = {};
 
-//   for (let key in obj) {
-//     clone[key] = makeDeepClone(obj[key]);
-//   }
-//   return clone;
-// }
-
-// makeDeepClone(obj);
+  for (let key in obj) {
+    clone[key] = makeDeepClone(obj[key]);
+  }
+  return clone;
+}
 
 // ===Task#2===
 function selectFromInterval(arr, x, y) {
@@ -46,3 +44,10 @@ function selectFromInterval(arr, x, y) {
 }
 
 // ===Task#3===
+function creaateIterable(from, to) {
+  let obj = [];
+  for (let i = from; i <= to; i += 1) {
+    obj.push(i);
+  }
+  return new Set(obj);
+}
